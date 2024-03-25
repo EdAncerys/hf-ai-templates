4e750710da83a0ae50535f8c7ca49118188ab017
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-03-25 18:06:41 +0000

    New commit message: add baner component & color base extention

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,43 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: '#f0f4ff',
+          200: '#d9e4ff',
+          300: '#a6c1ff',
+          400: '#598bff',
+          500: '#3366ff',
+          600: '#274bdb',
+          700: '#1a34b8',
+          800: '#102694',
+          900: '#091c7a',
+        },
+        secondary: {
+          100: '#f0f4ff',
+          200: '#d9e4ff',
+          300: '#a6c1ff',
+          400: '#598bff',
+          500: '#3366ff',
+          600: '#274bdb',
+          700: '#1a34b8',
+          800: '#102694',
+          900: '#091c7a',
+        },
+        tertiary: {
+          100: '#f0f4ff',
+          200: '#d9e4ff',
+          300: '#a6c1ff',
+          400: '#598bff',
+          500: '#3366ff',
+          600: '#274bdb',
+          700: '#1a34b8',
+          800: '#102694',
+          900: '#091c7a',
+        },
+      },
+    },
   },
   plugins: [
     // ...
